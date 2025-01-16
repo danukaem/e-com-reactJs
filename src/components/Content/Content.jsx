6c61432d24a66a7e8c321538d15905b79ef9f749
danukaem <danukaem@gmail.com>
2025-01-16 23:34:06 +0530

    routing done

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,17 +1,6 @@
 import React from "react";
 import "./Content.css";
-import {
-  Outlet,
-  Route,
-  BrowserRouter as Router,
-  Routes,
-} from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CartPage from "./pages/CartPage";
-import OrdersPage from "./pages/OrdersPage";
-import ProductPage from "./pages/ProductPage";
-import Header from "../Header/Header";
-import HeaderItems from "../Header/headerItems/HeaderItems";
+import { Outlet } from "react-router-dom";
 
 function Content() {
   return (
