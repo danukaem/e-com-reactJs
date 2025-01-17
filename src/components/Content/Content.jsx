637d3e5f4b03adc4bd2ead8dcdef0209c8675e86
danukaem <danukaem@gmail.com>
2025-01-17 10:03:02 +0530

    UseContext

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import "./Content.css";
 import { Outlet } from "react-router-dom";
 
+export const ContentContext = createContext();
+
 function Content() {
+  const [val, setVal] = useState("");
+
+  const typeVal = (e) => {
+    setVal(e.target.value);
+  };
+
   return (
     <>
-      <div className="content-style-1 ">
-        <Outlet />
-      </div>
+      <ContentContext.Provider value={val}>
+        <div className="content-style-1 ">
+          <Outlet />
+          <input type="text" onChange={typeVal} />
+        </div>
+      </ContentContext.Provider>
     </>
   );
 }
